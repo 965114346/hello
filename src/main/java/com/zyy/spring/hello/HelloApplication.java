8c7855830c30bf1aac6720a875c3e699ef969e97
yangyang.zhang <965114346@qq.com>
2019-04-07 23:35:03 +0800

    Initial commit

---
+++ src/main/java/com/zyy/spring/hello/HelloApplication.java
@@ -0,0 +1,27 @@
+package com.zyy.spring.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloApplication.class);
+    }
+
+    @GetMapping("/say")
+    public String say() {
+        return "Hello! Springboot war!";
+    }
+}
